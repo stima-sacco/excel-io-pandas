c184c9232a37954c4902b91e02a403f272dc8da2
Kevin Owino <kowino@STIMA-SAC.COM>
2022-07-28 17:29:23 +0300

    Parametrizing values

--- reader.py
+++ reader.py
@@ -3,8 +3,9 @@ import pandas as pd
 import warnings
 
 class data_cleanup(object):
-    def __init__(self, base_url):
-        self.base_url = base_url
+    def __init__(self, output_folders_source_url, output_folders_destination_url):
+        self.output_folders_source_url = output_folders_source_url
+        self.output_folders_destination_url = output_folders_destination_url
 
         self.branches = [
             'CBD',
@@ -28,25 +29,72 @@ class data_cleanup(object):
             'Branch_Code'
         ]
 
-    def create_branch_folders(self):
+        self.create_branch_folders_if()
+
+        for fl in os.listdir(self.output_folders_source_url):
+            file = ''.join([self.output_folders_source_url, '\\', fl])
+
+            self.create_brach_data_cleanup_output_file(file)
+
+    def create_branch_folders_if(self):
         for branch in self.branches:
-            fld_path = ''.join([self.base_url, '\\', branch])
+            fld_path = ''.join([self.output_folders_destination_url, '\\', branch])
 
             if os.path.exists(fld_path) == False:
                 os.mkdir(fld_path)
 
-    def create_brach_data_cleanup_output_file(self, file_name):
+    def get_valid_column_key(self, data_frame):
+        key_found = False
+        valid_id = ''
+
+        for id in self.branch_column_ids:
+            if id == 'Branch':
+                id = 'Branch'
+            
+            if id in data_frame.columns:
+                valid_id = id
+                key_found = True
+                break
+
+            else:
+                key_found = False
+                valid_id = ''
+
+        return_value = {
+            'key_found' : key_found,
+            'valid_id': valid_id
+        }
+
+        return return_value
+
+    def create_brach_data_cleanup_output_file(self, source_file_name):
         with warnings.catch_warnings(record=True):
             warnings.simplefilter("always")
-            df = pd.read_excel(r'C:\_Temp\DataCleanup\Members_with_Duplicate_P_I_N_NUMBERS.xls', dtype=object, index_col=0)#, engine='openpyxl')
 
-            data_frame = df.loc[df['Branch'] == 'CBD']
+            while True:
+                #source_file_name = ''.join([self.output_folders_source_url, '\\', file_name])
+                df = pd.read_excel(source_file_name, dtype=object, index_col=0)#, engine='openpyxl')
 
-            writer = pd.ExcelWriter('demo.xls', engine="openpyxl")
-            data_frame.to_excel(writer, sheet_name="Sheet1", index=False)
-            writer.save()
+                return_value = self.get_valid_column_key(df)
+
+                if return_value['key_found'] == False:
+                    break
+
+                valid_id = return_value['valid_id']
+                data_frame = df.loc[df[valid_id] == 'CBD']
+
+                
+                writer = pd.ExcelWriter('demo.xls', engine="openpyxl")
+                data_frame.to_excel(writer, sheet_name="Sheet1", index=False)
+                writer.save()
+                break
 
 
 if __name__ == '__main__':
-    base_url = r''
-    dc = data_cleanup(base_url)
+    output_folders_source_url = r'C:\_python\data_cleanup\Source'
+    output_folders_destination_url = r'C:\_python\data_cleanup\Destination'
+
+    dc = data_cleanup(
+        output_folders_source_url,
+        output_folders_destination_url
+    )
