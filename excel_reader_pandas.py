ed69f3cfbce50b30eec263052be99279ebdfff66
Kevin Owino <kowino@STIMA-SAC.COM>
2022-08-04 17:26:22 +0300

    Added Form UI

--- excel_reader_pandas.py
+++ excel_reader_pandas.py
@@ -1,6 +1,36 @@
 import os
 import pandas as pd
 import warnings
+import wx
+
+class Form(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, parent=None, title='Data Cleanup', size=(720,400))
+        self.Pan = wx.Panel(self, -1)
+
+        self.lblSourcePath = wx.StaticText(self.Pan, label='Source Path', pos=(10, 20), size=(70, 20))
+        self.txtSourcePath = wx.TextCtrl(self.Pan, pos=(110, 20), size=(430, 20))
+        self.btnSelectSourcePath = wx.Button(self.Pan, id=1, label='...', pos=(550, 20), size=(20,20))
+        self.btnSelectSourcePath.Bind(wx.EVT_BUTTON, self.evt_close) #subscribe to the event
+
+        self.lblDestinationPath = wx.StaticText(self.Pan, label='Destination Path', pos=(10, 45), size=(90, 20))
+        self.txtDestinationPath = wx.TextCtrl(self.Pan, pos=(110, 45), size=(430, 20))
+        self.btnSelectDestinationPath = wx.Button(self.Pan, id=1, label='...', pos=(550, 45), size=(20,20))
+        self.btnSelectDestinationPath.Bind(wx.EVT_BUTTON, self.evt_close) #subscribe to the event
+        
+        self.lblProgress = wx.StaticText(self.Pan, label='Progress', pos=(10, 80), size=(90, 20))
+        self.tcProgress = wx.TextCtrl(self.Pan, pos=(110, 105), size=(530, 70), style=wx.TE_MULTILINE|wx.TE_READONLY)
+        
+        self.lblNoColumnFiles = wx.StaticText(self.Pan, label='No Column Files', pos=(10, 130), size=(90, 20))
+        self.tcNoColumnFiles = wx.TextCtrl(self.Pan, pos=(110, 105), size=(530, 70), style=wx.TE_MULTILINE|wx.TE_READONLY)
+
+    def evt_close(self, evt):
+        self.Hide()
+
+if __name__ == '__main__':
+    app = wx.App()
+    f = Form().Show()
+    app.MainLoop()
 
 class data_cleanup(object):
     def __init__(self, output_folders_source_url, output_folders_destination_url):
@@ -96,7 +126,7 @@ class data_cleanup(object):
                     break
 
                 branch_name = os.path.basename(branch_folder_path)
-                # if branch_name == 'NONBRANCH':
+                # if branch_name == 'NONBRANCH':s
                 #     branch_name = 'NaN'
 
                 valid_id = return_value['valid_id']
@@ -116,12 +146,12 @@ class data_cleanup(object):
                 break
 
 
-if __name__ == '__main__':
-    output_folders_source_url = r'C:\_Temp\DataCleanup'
-    #output_folders_source_url = r'C:\_python\data_cleanup\Source'
-    output_folders_destination_url = r'C:\_python\data_cleanup\Destination'
+# if __name__ == '__main__':
+#     output_folders_source_url = r'C:\_Temp\DataCleanup'
+#     #output_folders_source_url = r'C:\_python\data_cleanup\Source'
+#     output_folders_destination_url = r'C:\_python\data_cleanup\Destination'
 
-    dc = data_cleanup(
-        output_folders_source_url,
-        output_folders_destination_url
-    )
+#     dc = data_cleanup(
+#         output_folders_source_url,
+#         output_folders_destination_url
+#     )
